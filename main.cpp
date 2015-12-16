a6a9c653177dfdf7dcc6e0c65e0d37d2c827a13d
lucassfara <lucassfara@yahoo.com>
2015-12-16 13:55:53 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main (){
+
+	string d;
+	int c=0;
+
+	cout << "Ingrese una cadena sin espacio" << endl;
+	cin >> d;
+
+	c=d.length();
+
+	cout << "Cantidad de caracteres de la cadena: " << c << endl;
+}
